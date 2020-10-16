dc11b18ae095e9974b0d735112bf19285378d4e7
Cameron Kenworthy <wavedanlandon@gmail.com>
2020-10-16 13:15:58 -0700

    using grid

--- hello.py
+++ hello.py
@@ -3,8 +3,11 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My Name is Cameron Kenworthy!")
 
-myLabel.pack()
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=0)
+
 
 root.mainloop()
