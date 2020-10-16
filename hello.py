83f1869e185f7da4c69ceccf718397a616e86961
Cameron Kenworthy <wavedanlandon@gmail.com>
2020-10-16 13:03:57 -0700

    hello world window

---
+++ hello.py
@@ -0,0 +1,10 @@
+#https://youtu.be/YXPyB4XeYLA
+from tkinter import *
+
+root = Tk()
+
+myLabel = Label(root, text="Hello World!")
+
+myLabel.pack()
+
+root.mainloop()
--- test.py
+++
@@ -1,1 +0,0 @@
-#https://youtu.be/YXPyB4XeYLA
